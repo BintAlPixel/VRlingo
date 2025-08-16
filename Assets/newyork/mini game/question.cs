9d92e8e0c01dafa53f3a88fb4320003b0f5446dc
maryam <104603611+BintAlPixel@users.noreply.github.com>
2025-08-16 23:12:23 +0300

    a

---
+++ Assets/newyork/mini game/question.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Question
+{
+    public Sprite img;   // your picture (with the word already on it)
+    public bool isTrue;  // the correct answer for that picture
+}
