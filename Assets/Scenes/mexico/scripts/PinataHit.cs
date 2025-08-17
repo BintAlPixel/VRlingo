da34b5405af2a61975f74439ece4d3b1ee0bc2d1
strgurl <jawhara57mohammed@gmail.com>
2025-08-17 04:54:01 +0300

    pinata

--- Assets/Scenes/mexico/scripts/PinataHit.cs
+++ Assets/Scenes/mexico/scripts/PinataHit.cs
@@ -1,39 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PinataHit : MonoBehaviour
 {
-    [Header("Objects that will fall when hit")]
-    public GameObject[] objectsToFall;  // المجسمات اللي تبغين تسقط
+    [Header("Candy Objects")]
+    public List<GameObject> candies;   // لستة الكانديز (خليهم في البداية SetActive = false)
 
-    [Header("Force applied to falling objects")]
-    public Vector3 fallForce = new Vector3(0, 2f, 2f); // دفعة للأوبجكتات عند الضربة
+    [Header("Audio")]
+    public AudioSource hitSound;       // صوت الضربة
+
+    [Header("Pinata Settings")]
+    public GameObject pinataObject;    // مجسم البنياتا نفسه
+    private int hitCount = 0;          // عدد الضربات
 
     void OnCollisionEnter(Collision collision)
     {
-        // تحقق إذا الاصطدام جاء من المضرب
+        // نتأكد إنه المضرب (مثلاً تاق "Bat")
         if (collision.gameObject.CompareTag("Bat"))
         {
-            // أضف قوة على البنياتا نفسها
-            Rigidbody rb = GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddForce(collision.relativeVelocity * 2f, ForceMode.Impulse);
-            }
-
-            // تفعيل الأوبجكتات وسقوطها
-            foreach (GameObject obj in objectsToFall)
-            {
-                if (obj != null)
-                {
-                    obj.SetActive(true); // يفعل المجسم لو كان مخفي
-                    Rigidbody objRb = obj.GetComponent<Rigidbody>();
-                    if (objRb != null)
-                    {
-                        objRb.isKinematic = false; // خلي الفيزياء تشتغل
-                        objRb.AddForce(fallForce, ForceMode.Impulse); // دفعة للأوبجكت
-                    }
-                }
-            }
+            HandleHit();
         }
     }
+
+    void HandleHit()
+    {
+        // نشغل الصوت
+        if (hitSound != null)
+            hitSound.Play();
+
+        // إذا باقي عندنا كانديز نخلي واحد يطيح
+        if (hitCount < candies.Count)
+        {
+            candies[hitCount].SetActive(true);  // نخليه يبان
+            hitCount++;
+        }
+        else
+        {
+            // إذا خلصوا الكانديز → نكسر/نخفي البنياتا
+            EndPinata();
+        }
+    }
+
+  void EndPinata()
+{
+    if (pinataObject != null)
+    {
+        // نشغل سكربت التأثير النهائي
+        PinataEndEffect endEffect = pinataObject.GetComponent<PinataEndEffect>();
+        if (endEffect != null)
+            endEffect.PlayEndEffect();
+    }
+
+    Debug.Log("Pinata Finished! 🎉");
+}
 }
